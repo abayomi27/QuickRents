93d7aaf3ac6d67f13fdef1208dc5c87ac9fb76aa
Abayomi Yusuf <yusufabayomi27@yahoo.com>
2017-12-21 22:57:30 +0100

    Carousel worked, no style yet

--- properties/views.py
+++ properties/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from properties.models import Properties
 
 # Create your views here.
@@ -6,4 +6,9 @@ from properties.models import Properties
 
 def homepage(request):
     properties = Properties.objects.all()
-    return render(request, 'properties/homepage.html', {'properties':properties})
+    return render(request, 'properties/homepage.html', {'properties': properties})
+
+
+def apartment_detail(request, pk):
+    apartment = get_object_or_404(Properties, pk=pk)
+    return render(request, 'properties/apartment_details.html', {'property': apartment})
